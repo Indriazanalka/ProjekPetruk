7a981c4bca456a0f55e50e4a5dfcb62c53f97ea2
Indriazanalka <151296105+Indriazanalka@users.noreply.github.com>
2024-05-29 20:40:36 +0700

    Update ImplementasiStack.cpp

--- ImplementasiStack.cpp
+++ ImplementasiStack.cpp
@@ -1,1 +1,36 @@
-#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+bool check(char,ch,vector<char>range);
+bool checkString(string str, vector<string>range);
+vector<string>convertTolnfix(string input);
+string removeWhitespace(string str);
+int checkPrecedence(string op);
+vector<string>
+convertToPostfix(vector<string>infix);
+double doOperation(double a, double b, string op);
+double evaluateOperation(vector<string>postfix);
+
+int main(){
+  string input;
+getline(cin,input);
+
+vector<string>infix=convertTolnfix(removeWhitespace(input));
+vector<string>postfix=convertTolPostfix(infix);
+
+  cout<<evaluateOperation(postfix)<<end;
+
+return 0;
+}
+
+bool check(char ch,vector<char>range){
+  vector<char>::iterator it;
+it = find(range.begin(),range end(),ch);
+return it - range.begin()<(long int) range size();
+}
+
+bool checkString(string str, vector>string>range){
+  vector<string>::iterator it;
+it = find(range.begin(),range.end(), str);
+return it - range.begin()<(long int) range.size();
+}
